236ecd07cc2596c4a2cd5b9d52f6b783e381809e
Andriele Joras dos Santos <andrielejorasdossantos@gmail.com>
2022-11-07 11:05:37 -0300

    :construction: Aula 3

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -8,14 +8,38 @@ namespace FilmesAPI.Controllers
     [ApiController]
     [Route("[controller]")]
 
-    public class FilmeController
+    public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
-        public void AdicionarFilme([FromBody]Filme filme)
+        public IActionResult AdicionarFilme([FromBody]Filme filme)
         {
+            filme.Id = id++;
             filmes.Add(filme);
+            // Busca o caminho do filme criado
+            return CreatedAtAction(nameof(RecuperarFilmesPorId), new {Id = filme.Id}, filme);
+
         }
+
+        [HttpGet]
+        public IActionResult RecuperarFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperarFilmesPorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+
+        
     }
 }
