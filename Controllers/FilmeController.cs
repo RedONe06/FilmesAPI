e73a9d40cb4415288e03bea07297effee0ef5612
Andriele Joras dos Santos <andrielejorasdossantos@gmail.com>
2022-11-07 09:24:12 -0300

    :tada: init

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class FilmeController
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+        }
+    }
+}
